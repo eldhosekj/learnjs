9bb6142e41574e81a4b1be807ae19cc100b94eec
Eldhose KJ <eldhosekannothukudi@gmail.com>
2021-11-22 05:19:52 +0530

    template literal

---
+++ 90_template literals.js
@@ -0,0 +1,21 @@
+const person={
+    name:"zodiac hasbro",
+    age: 56
+};
+const greeting='hello,myname is ${person.name}!
+i am ${person.age} years onload.';
+console.log(greeting);
+const result={
+    success:[max-length", "no-amd", "prefer-arrow-function"],
+failure:["no-var", "var-on-top","linebreak"],
+skipped:["id-blacklist","no-dup-keys"]
+};
+function makeList(arr){
+    const resultDisplayArray=null;
+    for(let i=0; i<arr.length;i++){
+        resultDisplayArray.push('<li class='<li class="text-warning">${arr[i]}')
+    }
+    return resultDisplayArray;
+}
+const resultDisplayArray=makeList(result.failure);
+console.log(resultDisplayArray)
