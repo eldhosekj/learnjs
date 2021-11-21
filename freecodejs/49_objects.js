dd180824865ee251c18c0d5b651c91b070a939a1
Eldhose KJ <eldhosekannothukudi@gmail.com>
2021-11-22 00:12:41 +0530

    object

---
+++ freecodejs/49_objects.js
@@ -0,0 +1,12 @@
+var ourDog={
+    "name": "camper",
+    "legs":4,
+    "tails":1,
+    "friends":["everything"]
+};
+var myDog={
+    "name":"quincy",
+    "legs":3,
+    "tails":2,
+    "friends":[]
+};
