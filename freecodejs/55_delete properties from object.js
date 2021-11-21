6db09da0ee16c1165c9d45353c7b5f1a81ee770b
Eldhose KJ <eldhosekannothukudi@gmail.com>
2021-11-22 00:42:51 +0530

    delete properties from objects

---
+++ freecodejs/55_delete properties from object.js
@@ -0,0 +1,15 @@
+var ourDog={
+    "name": "camper",
+    "legs":4,
+    "tails":1,
+    "friends":["everything"]
+};
+delete ourDog.bark
+var myDog={
+    "name":"quincy",
+    "legs":3,
+    "tails":2,
+    "friends":[]
+};
+myDog[bark]="woof"
+delete myDog.tails;
