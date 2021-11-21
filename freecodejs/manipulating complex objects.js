612a576c79aa266778dbd55b8498f1be75b2f70a
Eldhose KJ <eldhosekannothukudi@gmail.com>
2021-11-22 01:15:40 +0530

    manipulating complex objects

---
+++ freecodejs/manipulating complex objects.js
@@ -0,0 +1,21 @@
+var myMusic=[
+    {
+        "artist":"billy joel",
+        "title": "piano man",
+        "releaseyear":1973,
+        "formats": [
+            "cd",
+            "8t",
+            "lp"
+        ],
+        "gold":true
+    },
+    {
+        "artist":"beau carnes",
+        "title":"cereal man",
+        "releaseyear":2003,
+        "formats":[
+            "youtube vide"
+        ]
+    }
+]
